a04af69517313f8ed282ba1b25682d98dd88323f
Benjamin LaSelle <laselb61@uwosh.edu>
2021-04-26 10:50:39 -0500

    Added seller functionality

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,13 +11,18 @@ public class GUI extends Frame {
         Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
         int width = 200;
         int height = 200;
-        Button lower_buyer_prices = new Button("Raise Buyer Prices"); // Button is a component
-        add(lower_buyer_prices);
-        LowerBuyerPricesListener listener = new LowerBuyerPricesListener();
-        lower_buyer_prices.addActionListener(listener);
+        Button raise_buyer_prices = new Button("Raise Buyer Prices"); // Button is a component
+        add(raise_buyer_prices);
+        LowerBuyerPricesListener buyerListener = new LowerBuyerPricesListener();
+        raise_buyer_prices.addActionListener(buyerListener);
         setVisible(true);
         setSize(width,height);
         setBounds(center.x - width / 2, center.y - height / 2, width, height);
+        Button lower_seller_prices = new Button("Lower Seller Prices");
+        add(lower_seller_prices);
+        LowerSellerPricesListener sellerListener = new LowerSellerPricesListener();
+        lower_seller_prices.addActionListener(sellerListener);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
     public static void main(String[] args) {                // The Panel container adds a Button component
        GUI app = new GUI();
@@ -25,11 +31,11 @@ public class GUI extends Frame {
     private class LowerBuyerPricesListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-            String buyerFilePath = "C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\EverQuest\\userdata\\BART_YourCharacterName_server.ini";
-            File aabalaIni = new File(buyerFilePath);
+            String buyerFilePath = "C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\EverQuest\\userdata\\BART_Charname_server.ini";
+            File buyerIni = new File(buyerFilePath);
             Scanner scanner = null;
             try {
-                scanner = new Scanner(aabalaIni);
+                scanner = new Scanner(buyerIni);
             } catch (FileNotFoundException fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
             }
@@ -61,9 +67,9 @@ public class GUI extends Frame {
                         price++;
                     }
                     String newLine = data.substring(0, getSeventhCarrot(data) + 1) + price * 1000 + data.substring(getEighthCarrot(data));
-                    newString.append(newLine + System.lineSeparator());
+                    newString.append(newLine).append(System.lineSeparator());
                 } else {
-                    newString.append(data + System.lineSeparator());
+                    newString.append(data).append(System.lineSeparator());
                 }
             }
             System.out.println(newString);
@@ -92,6 +98,44 @@ public class GUI extends Frame {
                 index =  data.indexOf('^', index + 1);
             }
             return index;
+        }
+    }
+
+    private class LowerSellerPricesListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String sellerFilePath = "C:\\Users\\Public\\Daybreak Game Company\\Installed Games\\EverQuest\\BZR_Charname_server.ini";
+            File sellerIni = new File(sellerFilePath);
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(sellerIni);
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+            StringBuffer newString = new StringBuffer();
+            while (scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                if (!data.contains("ItemToSell")) {
+                    int price = Integer.parseInt(data.substring(data.indexOf('=') + 1)) / 1000;
+                    if (price < 1999999 && price > 10) {
+                        price = (int)(price * .99);
+                    }
+                    String newLine = data.substring(0, data.indexOf('=') + 1) + price * 1000;
+                    newString.append(newLine).append(System.lineSeparator());
+                } else {
+                    newString.append(data).append(System.lineSeparator());
+                }
+            }
+            System.out.println(newString);
+            scanner.close();
+            try {
+                FileWriter fileWriter = new FileWriter(sellerFilePath);
+                fileWriter.write(newString.toString());
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         }
     }
 }
